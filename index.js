5014231ddccdeda0717aceafa81da9ad45bfde4c
rakib <mdrakib991943@.com>
2026-03-06 22:51:38 +0600

    added  new form

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+//   const inputName = document.getElementById("inputName");
+// const inputPassword = document.getElementById("inputPassword");
+// const mainContainer = document.getElementById("mainContainer");
+// const loginForm = document.getElementById("loginForm");
+// console.log(loginForm);
+
+//  const handleSignIn=()=>{
+// const inputNameValue = inputName.value ;
+// const inputPasswordValue = inputPassword.value ;
+
+
+// if(inputNameValue ==="admin" && inputPasswordValue === "admin123" ){
+//     mainContainer.classList.remove("hidden");
+//     loginForm.classList.add("hidden");
+// }
+// }
+
+
+
+
